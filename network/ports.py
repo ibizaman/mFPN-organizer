56020220c049ec08e5eb4568a0dc3fbc8bd88bb1
Pierre Penninckx <ibizapeanut@gmail.com>
2016-10-17 04:23:12 +0200

    fix utils import in ports module

--- network/ports.py
+++ network/ports.py
@@ -18,7 +18,8 @@ import sys
 from docopt import docopt
 import yaml
 
-from utils import utils
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import utils
 
 
 def main(argv=None):
